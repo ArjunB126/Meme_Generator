951b4c6bd1b74fce38d2a40692cd93119f0c8f38
ArjunB126 <arjunbajare86@gmail.com>
2024-03-30 20:34:27 +0530

    Js initialization

---
+++ script.js
@@ -0,0 +1,5 @@
+const generate_meme_button = document.querySelector(".meme-generator .generate-meme-btn");
+const memeImg = document.querySelector(".meme-generator img");
+const memeTitle = document.querySelector(".meme-generator .meme-title");
+const memeAutor = document.querySelector(".meme-genrator .meme-author")
+
