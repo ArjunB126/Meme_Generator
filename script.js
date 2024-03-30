de200e40688c0cbfb5fbc2a6f5bcc541fd9062ee
ArjunB126 <arjunbajare86@gmail.com>
2024-03-30 20:42:11 +0530

    Js generate Meme function

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const generate_meme_button = document.querySelector(".meme-generator .generate-meme-btn");
+const generate_meme_button = document.querySelector(
+  ".meme-generator .generate-meme-btn"
+);
 const memeImg = document.querySelector(".meme-generator img");
 const memeTitle = document.querySelector(".meme-generator .meme-title");
-const memeAutor = document.querySelector(".meme-genrator .meme-author")
+const memeAutor = document.querySelector(".meme-genrator .meme-author");
 
+const generateMeme = () => {
+  fetch("https://meme-api.com/gimme/wholesomememes")
+    .then((response) => response.json())
+    .then((data) => {
+      updateDetails(data.url, data.title, data.author);
+    }); // Get a random wholesome meme from
+};
+
+generate_meme_button.addEventListener("click", generateMeme);
