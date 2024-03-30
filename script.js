71f201887d898924897188f3c4e728b397f28c8f
ArjunB126 <arjunbajare86@gmail.com>
2024-03-30 20:58:15 +0530

    Update Deatils Function and additional Spacing and formating

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const generateMemeBtn = document.querySelector(
     ".meme-generator .generate-meme-btn"
   );
+
   const memeImage = document.querySelector(".meme-generator img");
   const memeTitle = document.querySelector(".meme-generator .meme-title");
   const memeAuthor = document.querySelector(".meme-generator .meme-author");
   
+
+
   const updateDetails = (url, title, author) => {
     memeImage.setAttribute("src", url);
     memeTitle.innerHTML = title;
     memeAuthor.innerHTML = `Meme by: ${author}`;
   };
   
+
+
   const generateMeme = () => {
     fetch("https://meme-api.com/gimme/wholesomememes")
       .then((response) => response.json())
@@ -19,6 +24,9 @@ const generateMemeBtn = document.querySelector(
       });
   };
   
+
+
   generateMemeBtn.addEventListener("click", generateMeme);
+  
   
   generateMeme();
